004b0eae056434fbcba2d2feb7feb7be8239cfe5
benjabrevil6 <benjaminbrevil1701@gmail.com>
2023-04-07 14:00:49 -0400

    Mi primer commit

---
+++ 01-variable.py
@@ -0,0 +1,25 @@
+print("Hola soy Benjamín")
+
+#Declarando una variable
+nombre= "Benjamín"
+name= "Agustín"
+
+print(nombre)
+print("Hola soy", (nombre))
+print("Hola soy", (name))
+
+#Declarando una variable
+edad=19
+
+print("Mi edad es", (edad))
+
+#Imprimiendo 2 variables en una misma sentencia
+#print("Hola mi nombre es"+ nombre +"y mi edad es"+ edad +)
+print("Hola mi nombre es " + nombre + " y mi edad es " +str(edad))
+
+#Actualizando la variable nombre
+nombre1= "Andrés"
+
+#Utilizando la instruccion input
+nombre1=input("¿Cual es tu nuevo nombre?\n")
+print("Tu nombre es:",nombre1)
