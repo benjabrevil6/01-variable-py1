4597b9f8ee55680cfd839d9ca8d0789dcea80b81
benjabrevil6 <benjaminbrevil1701@gmail.com>
2023-06-28 23:28:23 -0400

    main

---
+++ retos/reto1.py
@@ -0,0 +1,5 @@
+nombre= input("¿Cual es tu nombre?\n")
+edad= input("¿Cual es tu edad?\n")
+total= 20 + int(edad)
+
+print(f"Hola mi nombre es {nombre}, tengo {edad} años y en 20 años tendre {total} años")
